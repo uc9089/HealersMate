bdad5e1593830011096e9b6db28b0bb6e3047dcd
Zeitung <119120139+uc9089@users.noreply.github.com>
2025-12-31 03:21:03 +0800

    Refactor profile settings for health and power bars
    Updated various profile settings for health and power bars, including styles, dimensions, and alignments.

--- ProfileManager.lua
+++ ProfileManager.lua
@@ -33,6 +33,9 @@ function InitializeDefaultProfiles()
 
     do
         local profile = CreateProfile("Long", "Base")
+
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
         profile.RaidMarkIcon.AlignmentH = "CENTER"
         profile.RaidMarkIcon.PaddingV = 0
         profile.RaidMarkIcon.OffsetY = 5
@@ -43,6 +46,8 @@ function InitializeDefaultProfiles()
         profile.RoleIcon.PaddingV = 0
         profile.RoleIcon.OffsetY = 5
         profile.RoleIcon.OffsetX = -5
+		
+		profile.BorderStyle = "Hidden"
     end
 
 
@@ -50,6 +55,8 @@ function InitializeDefaultProfiles()
     do
         local profile = CreateProfile("Long (Small)", "Base")
 
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
         profile.Width = 120
         profile.HealthBarHeight = 16
         profile.PowerBarHeight = 8
@@ -75,28 +82,47 @@ function InitializeDefaultProfiles()
         profile.RoleIcon.PaddingV = 0
         profile.RoleIcon.OffsetY = 5
         profile.RoleIcon.OffsetX = -4
+		
+		profile.BorderStyle = "Hidden"
     end
 
     do
         local profile = CreateProfile("Long (Integrated)", "Base")
-
-        profile.HealthBarHeight = 35
+		
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
+		profile.NameText.FontSize = 9
+        profile.HealthBarHeight = 27
+		profile.Width = 70
+		profile.PowerBarHeight = 3
+		profile.PowerDisplay = "Hidden"
         profile.PaddingBottom = 0
-        profile.AuraTracker.Height = 17
-        profile.AuraTracker.Width = 105
+        profile.AuraTracker.Height = 8
+        profile.AuraTracker.Width = 70
         profile.AuraTracker.Anchor = "Health Bar"
         profile.AuraTracker.AlignmentH = "LEFT"
         profile.TrackedAurasAlignment = "BOTTOM"
+		profile.TrackedAurasSpacing = 0
 
         profile.NameText.AlignmentV = "TOP"
         local healthTexts = profile.HealthTexts
         healthTexts.Normal.AlignmentV = "TOP"
-        healthTexts.WithMissing = util.CloneTable(healthTexts.Normal, true)
-        healthTexts.Missing.PaddingV = 4
+        healthTexts.Normal.FontSize = 9
+		healthTexts.WithMissing = util.CloneTable(healthTexts.Normal, true)
+        healthTexts.Missing.PaddingV = 0
 
-        profile.RaidMarkIcon.AlignmentH = "CENTER"
+        profile.IncomingHealDisplay = "Overheal"
+        profile.IncomingHealText.AlignmentH = "RIGHT"
+        profile.IncomingHealText.AlignmentV = "TOP"
+        profile.IncomingHealText.OffsetY = 2
+		profile.IncomingHealText.OffsetX = 0
+        profile.IncomingHealText.PaddingH = 3
+        profile.IncomingHealText.FontSize = 14
+
+        profile.RaidMarkIcon.AlignmentH = "LEFT"
         profile.RaidMarkIcon.PaddingV = 0
         profile.RaidMarkIcon.OffsetY = 5
+		profile.RaidMarkIcon.OffsetX = -8
         profile.RaidMarkIcon.Width = 14
         profile.RaidMarkIcon.Height = 14
 
@@ -104,21 +130,39 @@ function InitializeDefaultProfiles()
         profile.RoleIcon.PaddingV = 0
         profile.RoleIcon.OffsetY = 6
         profile.RoleIcon.OffsetX = -5
+		
+		profile.RangeText.AlignmentV = "BOTTOM"
+		profile.RangeText.AlignmentH = "CENTER"
+        profile.RangeText.OffsetY = 0
+        profile.RangeText.FontSize = 9
+		
+        profile.LineOfSightIcon.Width = 13
+        profile.LineOfSightIcon.Height = 13
+        profile.LineOfSightIcon.Anchor = "Health Bar"
+        profile.LineOfSightIcon.Opacity = 100
+		profile.LineOfSightIcon.AlignmentV = "TOP"
+		profile.LineOfSightIcon.AlignmentH = "RIGHT"
+		profile.LineOfSightIcon.OffsetX = 5
+		profile.LineOfSightIcon.OffsetY = 5
+		
+		profile.BorderStyle = "Hidden"
     end
 
     do
         local profile = CreateProfile("Small", "Base")
-
+		
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
         profile.Width = 67
-        profile.HealthBarHeight = 36
+        profile.HealthBarHeight = 32
         profile.NameText.FontSize = 11
         profile.NameText.AlignmentH = "CENTER"
         profile.NameText.AlignmentV = "TOP"
         profile.NameText.PaddingV = 1
-        profile.NameText.MaxWidth = 47
-        profile.PowerBarHeight = 6
+        profile.NameText.MaxWidth = 55
+        profile.PowerBarHeight = 2
         profile.PaddingBottom = 0
-        profile.AuraTracker.Height = 12
+        profile.AuraTracker.Height = 10
         profile.AuraTracker.Anchor = "Health Bar"
         profile.AuraTracker.AlignmentH = "LEFT"
         profile.TrackedAurasAlignment = "BOTTOM"
@@ -128,9 +172,9 @@ function InitializeDefaultProfiles()
         healthTexts.Normal.FontSize = 9
         healthTexts.Normal.AlignmentH = "CENTER"
         healthTexts.Normal.AlignmentV = "CENTER"
-        healthTexts.Normal.OffsetY = 2
+        healthTexts.Normal.OffsetY = 0
         healthTexts.WithMissing.FontSize = 9
-        healthTexts.WithMissing.AlignmentH = "LEFT"
+        healthTexts.WithMissing.AlignmentH = "CENTER"
         healthTexts.WithMissing.AlignmentV = "CENTER"
         healthTexts.WithMissing.OffsetY = 2
         healthTexts.Missing.FontSize = 9
@@ -141,13 +185,15 @@ function InitializeDefaultProfiles()
         profile.IncomingHealDisplay = "Overheal"
         profile.IncomingHealText.AlignmentH = "RIGHT"
         profile.IncomingHealText.AlignmentV = "CENTER"
-        profile.IncomingHealText.OffsetY = -6
-        profile.IncomingHealText.PaddingH = 2
-        profile.IncomingHealText.FontSize = 7
+        profile.IncomingHealText.OffsetY = -3
+        profile.IncomingHealText.PaddingH = 3
+        profile.IncomingHealText.FontSize = 9
 
+        profile.RangeText.AlignmentH = "LEFT"
         profile.RangeText.AlignmentV = "CENTER"
-        profile.RangeText.OffsetY = -6
-        profile.RangeText.FontSize = 8
+        profile.RangeText.OffsetY = -3
+        profile.RangeText.PaddingH = 3
+        profile.RangeText.FontSize = 9
         profile.LineOfSightIcon.Width = 20
         profile.LineOfSightIcon.Height = 20
         profile.LineOfSightIcon.Anchor = "Health Bar"
@@ -161,21 +207,35 @@ function InitializeDefaultProfiles()
         profile.MissingHealthDisplay = "-Health"
         profile.PowerDisplay = "Hidden"
         profile.PowerText.FontSize = 8
+		
+		profile.BorderStyle = "Hidden"
+    end
+
+    do
+        local profile = CreateProfile("Small (No Buffs)", "Small")
+        profile.HealthBarHeight = 26
+        profile.AuraTracker.Height = 1
+		local healthTexts = profile.HealthTexts
+        healthTexts.Normal.OffsetY = -4
+
     end
 
     do
         local profile = CreateProfile("Very Small", "Base")
-
+		
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
         profile.Width = 50
         profile.HealthBarHeight = 29
-        profile.NameText.FontSize = 9
+        profile.NameText.FontSize = 10
         profile.NameText.AlignmentH = "LEFT"
         profile.NameText.AlignmentV = "TOP"
-        profile.NameText.PaddingV = 1
-        profile.NameText.OffsetX = 6
-        profile.NameText.MaxWidth = 34
+        profile.NameText.PaddingV = 0
+        profile.NameText.OffsetX = -2
+        profile.NameText.MaxWidth = 50
         profile.NameText.Color = "Class"
-        profile.PowerBarHeight = 5
+
+        profile.PowerBarHeight = 1
         profile.PaddingBottom = 0
         profile.AuraTracker.Height = 11
         profile.AuraTracker.Anchor = "Health Bar"
@@ -188,13 +248,18 @@ function InitializeDefaultProfiles()
         healthTexts.Normal.AlignmentH = "CENTER"
         healthTexts.Normal.AlignmentV = "CENTER"
         healthTexts.Normal.OffsetY = 1
+		
+		healthTexts.WithMissing.FontSize = 1
+        healthTexts.WithMissing.AlignmentH = "LEFT"
+        healthTexts.WithMissing.AlignmentV = "CENTER"
+        healthTexts.WithMissing.OffsetY = 2
 
         profile.IncomingHealDisplay = "Hidden"
-        profile.IncomingHealText.AlignmentH = "RIGHT"
+        profile.IncomingHealText.AlignmentH = "CENTER"
         profile.IncomingHealText.AlignmentV = "CENTER"
-        profile.IncomingHealText.OffsetY = -6
-        profile.IncomingHealText.PaddingH = 2
-        profile.IncomingHealText.FontSize = 7
+        profile.IncomingHealText.OffsetY = 0
+        profile.IncomingHealText.PaddingH = 0
+        profile.IncomingHealText.FontSize = 1
 
         profile.RangeText.AlignmentV = "CENTER"
         profile.RangeText.OffsetY = -6
@@ -215,6 +280,8 @@ function InitializeDefaultProfiles()
         profile.PowerDisplay = "Hidden"
         profile.PowerText.FontSize = 8
         profile.Orientation = "Vertical"
+		
+		profile.BorderStyle = "Hidden"
     end
 
     do
@@ -224,7 +291,9 @@ function InitializeDefaultProfiles()
 
     do
         local profile = CreateProfile("Default", "Base")
-
+		
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
         profile.Width = 100
         profile.HealthBarHeight = 36
         profile.PowerBarHeight = 9
@@ -233,6 +302,7 @@ function InitializeDefaultProfiles()
         profile.NameText.AlignmentV = "TOP"
         profile.NameText.PaddingV = 1
         profile.NameText.MaxWidth = 80
+		
         profile.PaddingBottom = 0
         profile.AuraTracker.Height = 14
         profile.AuraTracker.Anchor = "Health Bar"
@@ -273,11 +343,15 @@ function InitializeDefaultProfiles()
         profile.MissingHealthDisplay = "-Health"
         profile.PowerText.FontSize = 8
         profile.PowerText.AlignmentH = "CENTER"
+		
+		profile.BorderStyle = "Hidden"
     end
 
     do
         local profile = CreateProfile("Default (Short Bar)", "Base")
-
+		
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
         profile.Width = 100
         profile.HealthBarHeight = 24
         profile.PowerBarHeight = 9
@@ -290,7 +364,7 @@ function InitializeDefaultProfiles()
         profile.PaddingTop = 12
         profile.PaddingBottom = 0
         profile.AuraTracker.Height = 13
-        profile.AuraTracker.Anchor = "Health Bar"
+		profile.AuraTracker.Anchor = "Health Bar"
         profile.AuraTracker.AlignmentH = "LEFT"
         profile.TrackedAurasAlignment = "BOTTOM"
         profile.TrackedAurasSpacing = 1
@@ -330,41 +404,106 @@ function InitializeDefaultProfiles()
         profile.MissingHealthDisplay = "-Health"
         profile.PowerText.FontSize = 8
         profile.PowerText.AlignmentH = "CENTER"
+		
+		profile.BorderStyle = "Hidden"
+    end
+	
+	do
+        local profile = CreateProfile("Hidden", "Base")
+		
+		profile.HealthBarStyle = "Blizzard Raid Sideless"
+		profile.PowerBarStyle = "Blizzard Raid Sideless"
+        profile.Width = 0
+        profile.HealthBarHeight = 0
+        profile.PowerBarHeight = 0
+        profile.NameText.FontSize = 1
+        profile.NameText.AlignmentH = "CENTER"
+        profile.NameText.AlignmentV = "TOP"
+        profile.NameText.PaddingV = 0
+        profile.NameText.MaxWidth = 0
+        profile.NameText.Anchor = "Container"
+        profile.PaddingTop = 0
+        profile.PaddingBottom = 0
+        profile.AuraTracker.Height = 0
+		profile.AuraTracker.Anchor = "Health Bar"
+        profile.AuraTracker.AlignmentH = "LEFT"
+        profile.TrackedAurasAlignment = "BOTTOM"
+        profile.TrackedAurasSpacing = 0
+
+        local healthTexts = profile.HealthTexts
+        healthTexts.Normal.FontSize = 1
+        healthTexts.Normal.AlignmentH = "CENTER"
+        healthTexts.Normal.AlignmentV = "TOP"
+        healthTexts.Normal.OffsetY = 0
+        healthTexts.Normal.PaddingV = 0
+        healthTexts.WithMissing.FontSize = 1
+        healthTexts.WithMissing.AlignmentH = "CENTER"
+        healthTexts.WithMissing.AlignmentV = "TOP"
+        healthTexts.WithMissing.OffsetY = 0
+        healthTexts.WithMissing.PaddingH = 0
+        healthTexts.Missing.FontSize = 1
+        healthTexts.Missing.AlignmentH = "RIGHT"
+        healthTexts.Missing.AlignmentV = "TOP"
+        healthTexts.Missing.OffsetY = 0
+        healthTexts.Missing.PaddingH = 0
+        
+        profile.IncomingHealDisplay = "Overheal"
+        profile.IncomingHealText.AlignmentH = "LEFT"
+        profile.IncomingHealText.AlignmentV = "TOP"
+        profile.IncomingHealText.OffsetY = 0
+        profile.IncomingHealText.PaddingH = 0
+        profile.IncomingHealText.PaddingV = 0
+
+        profile.RangeText.AlignmentV = "CENTER"
+        profile.RangeText.OffsetY = -0
+        profile.RangeText.FontSize = 1
+        profile.LineOfSightIcon.Width = 0
+        profile.LineOfSightIcon.Height = 0
+        profile.LineOfSightIcon.Anchor = "Health Bar"
+        profile.LineOfSightIcon.Opacity = 0
+        profile.HealthDisplay = "Health"
+        profile.MissingHealthDisplay = "-Health"
+        profile.PowerText.FontSize = 1
+        profile.PowerText.AlignmentH = "CENTER"
+		
+		profile.BorderStyle = "Hidden"
     end
 
     -- Legacy profile - Meant to look as close as possible to HealersMate 1.3.0
     do
         local profile = CreateProfile("Legacy", "Base")
 
-        profile.Width = 200
+        profile.Width = 100
 
-        profile.PaddingTop = 20
+        profile.PaddingTop = 0
 
         profile.MissingHealthInline = true
         profile.HealthBarHeight = 25
-        profile.HealthBarStyle = "Blizzard"
-        profile.PowerBarHeight = 5
-        profile.PowerBarStyle = "Blizzard"
+        profile.HealthBarStyle = "Blizzard Raid Sideless"
+        profile.PowerBarHeight = 3
+        profile.PowerBarStyle = "Blizzard Raid Sideless"
 
         profile.NameText.AlignmentH = "LEFT"
         profile.NameText.AlignmentV = "TOP"
         profile.NameText.Anchor = "Container"
         profile.NameText.MaxWidth = 200
         local healthTexts = profile.HealthTexts
-        healthTexts.Normal.AlignmentH = "CENTER"
-        profile.HealthDisplay = "Health/Max Health"
+        healthTexts.Normal.AlignmentH = "RIGHT"
+        profile.HealthDisplay = "Health"
+        profile.MissingHealthDisplay = "-Health"
         profile.PowerDisplay = "Hidden"
 
         profile.IncomingHealDisplay = "Overheal"
-        profile.IncomingHealText.AlignmentH = "LEFT"
+        profile.IncomingHealText.AlignmentH = "CENTER"
         profile.IncomingHealText.AlignmentV = "CENTER"
 
-        profile.RoleIcon.AlignmentH = "RIGHT"
+        profile.RoleIcon.AlignmentH = "CENTER"
         profile.RoleIcon.Width = 16
         profile.RoleIcon.Height = 16
 
-        profile.RaidMarkIcon.AlignmentH = "RIGHT"
-        profile.RaidMarkIcon.OffsetX = -18
+        profile.RaidMarkIcon.AlignmentH = "CENTER"
+        profile.RaidMarkIcon.OffsetX = -50
+		profile.RaidMarkIcon.OffsetY = 7
         profile.RaidMarkIcon.Width = 16
         profile.RaidMarkIcon.Height = 16
 
